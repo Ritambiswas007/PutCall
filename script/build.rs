b17037a21a51a07a6107691417f78fbd9ebd5814
Titan13 <114689483+Ritambiswas007@users.noreply.github.com>
2025-07-30 22:50:44 +0530

    Update build.rs

--- script/build.rs
+++ script/build.rs
@@ -1,12 +1,18 @@
+use std::env;
 use sp1_build::build_program_with_args;
 
 fn main() {
+    // Skip building guest programs if proving remotely
+    let prover = env::var("SP1_PROVER").unwrap_or_default();
+    if prover == "network" {
+        println!("cargo:warning=Skipping guest program build since SP1_PROVER=network");
+        return;
+    }
+
+    // Only build when proving locally
     build_program_with_args("../program", Default::default());
     build_program_with_args("../Icr", Default::default());
     build_program_with_args("../Liquid", Default::default());
     build_program_with_args("../Real_time_ltv", Default::default());
-    
-    
     build_program_with_args("../BimaOccumPutCall", Default::default());
-
 }
