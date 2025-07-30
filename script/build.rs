0090a1fbcb061eeaa83479d61f170ea1b8d9dd3c
Titan13 <114689483+Ritambiswas007@users.noreply.github.com>
2025-07-30 23:34:03 +0530

    Update build.rs

--- script/build.rs
+++ script/build.rs
@@ -1,12 +1,10 @@
 use sp1_build::build_program_with_args;
 
 fn main() {
+    std::env::set_var("RUSTFLAGS", "-C linker=rust-lld");
     build_program_with_args("../program", Default::default());
     build_program_with_args("../Icr", Default::default());
     build_program_with_args("../Liquid", Default::default());
     build_program_with_args("../Real_time_ltv", Default::default());
-    
-    
     build_program_with_args("../BimaOccumPutCall", Default::default());
-
 }
