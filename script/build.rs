5130074c8d9d9856986c6b32884d5b6e2666b86d
Titan13 <114689483+Ritambiswas007@users.noreply.github.com>
2025-07-31 00:17:49 +0530

    Update build.rs

--- script/build.rs
+++ script/build.rs
@@ -1,7 +1,6 @@
 use sp1_build::build_program_with_args;
 
 fn main() {
-    std::env::set_var("RUSTFLAGS", "-C linker=rust-lld");
     build_program_with_args("../program", Default::default());
     build_program_with_args("../Icr", Default::default());
     build_program_with_args("../Liquid", Default::default());
